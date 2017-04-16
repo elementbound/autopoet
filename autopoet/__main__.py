bdf3885e275a263010576803ec7fdf51a0316a26
Gálffy Tamás <ezittgtx@gmail.com>
2017-04-17 02:36:49 +0300

    Mad crashing was because wrong codepage; added code to warn about it

--- autopoet/__main__.py
+++ autopoet/__main__.py
@@ -5,6 +5,7 @@ from pathlib import Path
 os.chdir(str(Path('.').absolute().parent))
 
 import sys
+import platform
 import argparse
 
 import autopoet.demos.wordstats as wordstats
@@ -13,6 +14,14 @@ import autopoet.demos.autosuggest as autosuggest
 import autopoet.poetcrawler as poetcrawler
 
 def main():
+    # Fix up code-page
+    if platform.system() == 'Windows':
+        if sys.stdout.encoding != 'cp65001':
+            print('Detected stdout encoding', sys.stdout.encoding)
+            print('Setting it to cp65001')
+            os.system('chcp 65001')
+            sys.stdout.flush()
+
     demos = [wordstats, autosuggest]
     demo_names = [demo.__name__.split('.')[-1] for demo in demos]
 
